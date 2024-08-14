41f6fef20677ddf60970382628876b375991c042
markhallman <markhallman@illumon.com>
2024-08-14 17:20:43 -0500

    Downloads page formatting

--- src/pages/downloads.tsx
+++ src/pages/downloads.tsx
@@ -6,7 +6,31 @@ function Downloads(){
     return (
         <>
             <h1><Banner pages={downloadsPages} activePage="Downloads" /></h1>
-            <p> Downloads page</p>
+            <div className="content-area p-3">
+                <div className="wrapper">
+                    <p>
+                        Right now, a downloadable hearts client is only available for MacOS, we are working on a windows version.
+                        We are not paying apple for a developer certificate because it is stupid and expensive,
+                        so you may have to manually allow the application through system preferences if Mac is preventing you
+                        from opening it
+                    </p>
+                    <div className="downloadCards">
+                        <div className="card p-2" style={{width:300, height:200}}>
+                          <div className="card-body">
+                            <h5 className="card-title">MacOS Hearts Client</h5>
+                            <p className="card-text"> Hearts client compatible with MacOS, must be on version 14.0 or later</p>
+                            <a href="../resources/clientversions/HeartsClient-1.0.0.dmg" className="btn btn-primary">HeartsClient-1.0.0.dmg</a>
+                          </div>
+                        </div>
+                        <div className="card p-2" style={{width:300, height:200}}>
+                          <div className="card-body">
+                            <h5 className="card-title">Windows Hearts Client</h5>
+                            <p className="card-text"> COMING SOON</p>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,7 +6,7 @@ function Home(){
     return (
         <>
             <h1><Banner pages={homepages} activePage="Home" /></h1>
-            <p> Welcome to Coolest Card Games! A place for card game fans from all over the world to play the coolest
+            <p className="p-2"> Welcome to Coolest Card Games! A place for card game fans from all over the world to play the coolest
             card games. Really, right now thats just hearts, but what's cooler than hearts?</p>
         </>
     );
